f7a8c129d1bda296a39217d1f0bc695d68862a70
Pablo Moreno <pablo.moreno@thekeytalent.com>
2019-07-24 11:14:15 +0200

    Fix: Running command after success response sent

--- apps/views.py
+++ apps/views.py
@@ -5,17 +5,25 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from apps.models import WebHook
+from apps.utils import AfterResponseAction
 
 
 class WebhookHandler(APIView):
+    def release(self, webhook):
+        def run_command():
+            out = StringIO()
+            sys.stdout = out
+            call_command('deploy_application', app=webhook.repository, app_version=webhook.version, stdout=out)
+        return run_command
+
     def post(self, request):
         webhook = WebHook.from_request(request)
         webhook.save()
 
         if webhook.type == 'release':
-            out = StringIO()
-            sys.stdout = out
-            call_command('deploy_application', app=webhook.repository, app_version=webhook.version, stdout=out)
+            return AfterResponseAction({
+                'status': HTTP_200_OK,
+            }, after_response_action=self.release(webhook))
 
         return Response({
             'status': HTTP_200_OK,
