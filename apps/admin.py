7821a7193110b1c9f9097893e11c0b7b1e803e3e
Pablo Moreno <pablo.moreno@thekeytalent.com>
2019-07-26 12:32:59 +0200

    Added Secret model to verify Github secret

--- apps/admin.py
+++ apps/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from apps.models import Application, WebHook, Deploy
+from apps.models import Application, WebHook, Deploy, Secret
 
 admin.site.register(Application)
 admin.site.register(Deploy)
 admin.site.register(WebHook)
+admin.site.register(Secret)
--- apps/models.py
+++ apps/models.py
@@ -1,4 +1,28 @@
+import hmac
 from django.db import models
+from string import digits, ascii_letters
+from random import choice
+
+
+class Secret(models.Model):
+    app = models.CharField(max_length=32)
+    secret = models.CharField(max_length=32, blank=True, null=True, db_index=True)
+
+    def verify_signature(self, request):
+        signature = request.headers.get('X-Hub-Signature', None)
+
+        if not signature:
+            raise Exception('Signature not found')
+
+        sha_name, sign = signature.split('=')
+        mac = hmac.new(self.secret, msg=request.data, digestmod='sha1')
+
+        return hmac.compare_digest(mac.hexdigest(), sign)
+
+    def save(self, *args, **kwargs):
+        if not self.secret:
+            self.secret = ''.join([choice(digits + ascii_letters) for _ in range(0, 32)])
+        super().save(*args, **kwargs)
 
 
 class Application(models.Model):
@@ -23,6 +47,12 @@ class WebHook(models.Model):
 
     @staticmethod
     def from_request(request):
+        secret = Secret.objects.get(app='Github')
+        verified = secret.verify_signature(request)
+
+        if not verified:
+            raise Exception('Invalid signature')
+
         webhook = WebHook()
         webhook_type = request.META.get('HTTP_X_GITHUB_EVENT', None)
         data = request.data
